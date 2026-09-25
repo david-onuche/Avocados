4b6e3487adb8d6233564fb63a258b4cb85bfd7e3
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 19:56:43 +0100

    Modify Order

---
+++ Avocados/View/FactsView.swift
@@ -0,0 +1,18 @@
+//
+//  FactsView.swift
+//  Avocados
+//
+//  Created by David Onuche on 25/09/2026.
+//
+
+import SwiftUI
+
+struct FactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FactsView()
+}
