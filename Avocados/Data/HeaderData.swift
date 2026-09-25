ba2249d34b47c446491e34efdec9794a4a847ef0
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 11:59:34 +0100

    featch data

---
+++ Avocados/Data/HeaderData.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderData.swift
+//  Avocados
+//
+//  Created by David Onuche on 25/09/2026.
+//
+
+import Foundation
---
+++ Avocados/Model/HeaderModel.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderModel.swift
+//  Avocados
+//
+//  Created by David Onuche on 25/09/2026.
+//
+
+import Foundation
