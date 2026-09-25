95875966e8da81c32d204d3d36866cdf42df4db3
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 20:12:19 +0100

    Fetch Data

--- Avocados/Model/FactModel.swift
+++ Avocados/Model/FactModel.swift
@@ -5,4 +5,12 @@
 //  Created by David Onuche on 25/09/2026.
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - FACT MODEL
+
+struct Fact: Identifiable {
+    var id = UUID()
+    var image: String
+    var content: String
+}
