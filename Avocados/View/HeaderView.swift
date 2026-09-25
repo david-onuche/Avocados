c4ddf34e28682d6ceb6248e4a486f4598058b46e
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 11:34:05 +0100

    Layout for the header.

---
+++ Avocados/View/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Avocados
+//
+//  Created by David Onuche on 25/09/2026.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
