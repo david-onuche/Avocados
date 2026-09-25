4d7e7629884bbc65fa23fa697c6245404dc380eb
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 19:18:12 +0100

    Update

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,17 +9,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     // MARK: - PROPERTIES
-    
-    
     var headers: [Header] = headersData
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .center, spacing: 20) {
-                // MARK: - HEADER
                 
-                   
+                
+                // MARK: - HEADER
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(alignment: .top, spacing: 0) {
                             ForEach(headers) { item in
@@ -27,19 +26,28 @@ struct ContentView: View {
                         }
                         }
                     }
+                
+                  
+                // MARK: - DISHES
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                DishesView()
+                    .frame(maxWidth: 640)
+                
                     
                     // MARK: - FOOTER
                     VStack(alignment: .center, spacing: 20) {
                         Text("All About Avocados")
-                            .font(.system(.title, design: .serif))
                             .fontWeight(.bold)
-                            .foregroundColor(Color("ColorGreenAdaptive"))
-                            .padding(8)
+                            .modifier(TitleModifier())
                         
                         Text("Everything you wanted to know about avocados but were too afraid to ask.")
                             .font(.system(.body, design: .serif))
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color.gray)
+                            .frame(minHeight: 60)
                     }
                     .frame(maxWidth: 640)
                     .padding()
@@ -50,6 +58,15 @@ struct ContentView: View {
             .padding(0)
         }
     }
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
