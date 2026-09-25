8dc05b22b96c723c1fc6cb5ae8fcc7c8b0b61396
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 10:41:50 +0100

    Updated

--- Avocados/View/AppView.swift
+++ Avocados/View/AppView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct AppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            AvocadosView()
+                .tabItem({
+                   Image("tabicon-branch")
+                    Text("Avocados")
+                })
+            ContentView()
+                .tabItem({
+                    Image("tabicon-book")
+                    Text("Recipes")
+                })
+            RipeningStagesView()
+                .tabItem({
+                    Image("tabicon-avocado")
+                    Text("Ripening")
+                })
+            SettingsView()
+                .tabItem({
+                    Image("tabicon-settings")
+                    Text("Settings")
+                })
+        }
+        .edgesIgnoringSafeArea(.top)
+        .accentColor(Color.primary)
     }
 }
 
-#Preview {
-    AppView()
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
 }
--- Avocados/View/RipeningStagesView.swift
+++ Avocados/View/RipeningStagesView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct RipeningStagesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Ripening Stage")
     }
 }
 
-#Preview {
-    RipeningStagesView()
+struct RipeningStagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStagesView()
+    }
 }
