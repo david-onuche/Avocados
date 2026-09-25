0c7bcb2ccaf668c79395e09615e78251ebf5e748
davidonuche <iamdavidonuche@gmail.com>
2026-09-25 20:12:02 +0100

    Fetch Data

---
+++ Avocados/Data/FactData.swift
@@ -0,0 +1,8 @@
+//
+//  FactData.swift
+//  Avocados
+//
+//  Created by David Onuche on 25/09/2026.
+//
+
+import Foundation
---
+++ Avocados/Model/FactModel.swift
@@ -0,0 +1,8 @@
+//
+//  FactModel.swift
+//  Avocados
+//
+//  Created by David Onuche on 25/09/2026.
+//
+
+import Foundation
